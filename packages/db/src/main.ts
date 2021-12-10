fb8703f7ca90b6e37d8c22e12549288ef83bc5e0
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-10 22:17:26 +0300

    Initial lowdb

--- packages/db/src/main.ts
+++ packages/db/src/main.ts
@@ -1,1 +1,23 @@
-console.log('hello from @rana/db');
+import { JSONFile, Low } from "lowdb";
+
+type Data = {
+    posts: string[];
+}
+
+const main = async () => {
+    const adapter = new JSONFile<Data>('./storage/db.json')
+    const db = new Low<Data>(adapter)
+    await db.read();
+
+    console.log(db.data)
+
+    db.data
+        .posts
+        .push('hello again');
+    db.write();
+
+    console.log(db.data)
+
+};
+
+main();
