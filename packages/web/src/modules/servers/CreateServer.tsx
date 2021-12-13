74f4b4302ad8116a3acc0a3187c9231be56f5ad6
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-13 15:28:42 +0300

    Add CreateServer view

--- packages/web/src/modules/servers/CreateServer.tsx
+++ packages/web/src/modules/servers/CreateServer.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import cn from 'classnames';
+
 import Cores from '@modules/cores/Cores';
 import GameVersions from '@modules/game-versions/GameVersions';
 import VersionTypes from '@modules/version-types/VersionTypes';
+import Button from '@ui/Button';
+
+import styles from './CreateServer.module.css';
 
 const CreateServer = () => {
   return (
-    <div>
-      <VersionTypes />
-      <hr />
-      <GameVersions />
-      <Cores />
+    <div className={cn(styles.createServer)}>
+      <section className={cn(styles.section)}>
+        <VersionTypes />
+      </section>
+      <section className={cn(styles.section)}>
+        <GameVersions />
+      </section>
+      <section className={cn(styles.section)}>
+        <Cores />
+      </section>
+      <section className={cn(styles.section)}>
+        <Button text="Create Server" />
+      </section>
     </div>
   );
 };
