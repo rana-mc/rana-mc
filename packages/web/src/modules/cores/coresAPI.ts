f2d7ec786ea6bb14b603fb26f51fafe101c06d52
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-13 09:35:11 +0300

    Remove forgotten force=1 of coresAPI

--- packages/web/src/modules/cores/coresAPI.ts
+++ packages/web/src/modules/cores/coresAPI.ts
@@ -1,10 +1,10 @@
 import { apiClient } from "@utils";
 
 export const fetchForgeCores = (gameVersion: string) => {
-  return apiClient.get('/api/forge-cores', { params: { version: gameVersion, force: 1 } });
+  return apiClient.get('/api/forge-cores', { params: { version: gameVersion } });
 };
 
 
 export const fetchFabricCores = (gameVersion: string) => {
-  return apiClient.get('/api/fabric-cores', { params: { version: gameVersion, force: 1 } });
+  return apiClient.get('/api/fabric-cores', { params: { version: gameVersion } });
 };
