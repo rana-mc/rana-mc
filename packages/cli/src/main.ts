994169b641a06375c42a3977a0b456ef45a3610e
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-14 16:07:46 +0300

    Initial @rana-mc/cli

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,1 +1,24 @@
-console.log('hello from @rana-mc/cli');
+import express from 'express';
+import path from 'path';
+import { startApiServer } from '@rana-mc/api';
+
+const WEB_PORT = 3000;
+
+const startWebServer = async () => {
+  const app = express();
+  const webPath = path.resolve('.', 'node_modules/@rana-mc/web/dist');
+
+  console.log(`RanaMC web path: ${webPath}`);
+  app.use(express.static(webPath));
+
+  app.listen(WEB_PORT, () => {
+    console.log(`Working on ${WEB_PORT} port...`);
+  });
+};
+
+const main = () => {
+  startWebServer();
+  startApiServer();
+};
+
+main();
