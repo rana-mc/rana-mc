9bb05e95e67560cb0461f8b9cf972bbcfde2ca51
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-15 22:22:40 +0300

    Use FabricServer too

--- packages/api/src/RanaAPI/apis/install.ts
+++ packages/api/src/RanaAPI/apis/install.ts
@@ -1,6 +1,12 @@
 import { ForgeServer } from '@rana-mc/forge';
+import { FabricServer } from '@rana-mc/fabric';
 import RanaDB, { ranaDB } from '../../RanaDB/RanaDB';
 import APIRoute from '../APIRoute';
+
+enum ServerCoreType {
+  Forge = 'forge',
+  Fabric = 'fabric',
+}
 
 export default class InstallAPI extends APIRoute {
 
@@ -27,7 +33,14 @@ export default class InstallAPI extends APIRoute {
       const server = await ranaDB.findServer(serverId);
 
       this.log(`Installing server with id = ${serverId}`);
-      new ForgeServer(server).installCore();
+
+      if (server.core.type === ServerCoreType.Forge) {
+        new ForgeServer(server).installCore();
+      }
+
+      if (server.core.type === ServerCoreType.Fabric) {
+        new FabricServer(server).installCore();
+      }
 
       const servers = await this.ranaDB.getServers();
       res.send(servers);
