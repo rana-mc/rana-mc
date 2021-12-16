87d7849480b3fbee575dae78fa89148dd7ba1035
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-16 09:49:10 +0300

    Base event for RanaSocket

--- packages/api/src/RanaAPI/socket/RanaSocket.ts
+++ packages/api/src/RanaAPI/socket/RanaSocket.ts
@@ -1,19 +1,32 @@
 import { Server as SocketIOServer } from 'socket.io'
 import { Server as HTTPServer } from 'http'
+import { Logger } from '../Logger';
 
 export default class RanaSocket {
 
+  public static TAG = 'RanaSocket';
+
   socket: SocketIOServer
+  logger: Logger = new Logger(RanaSocket.TAG)
 
   constructor(server: HTTPServer) {
-    this.socket = new SocketIOServer(server);
+    this.socket = new SocketIOServer(server, {
+      cors: {
+        origin: "*"
+      }
+    });
     this.init();
   }
 
   init() {
     this.socket.on('connection', client => {
-      client.on('event', data => { /* … */ });
-      client.on('disconnect', () => { /* … */ });
+      client.on('disconnect', () => {
+        this.logger.log('disconnect');
+      });
+
+      client.on('hello', (message: string) => {
+        this.logger.log(message);
+      })
     });
   }
 }
