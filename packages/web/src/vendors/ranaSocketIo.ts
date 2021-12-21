b67f6ba19d386b6f8496d4c7cc33aa22af1ed5d1
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-21 12:46:02 +0300

    Fix socket load error

--- packages/web/src/vendors/ranaSocketIo.ts
+++ packages/web/src/vendors/ranaSocketIo.ts
@@ -1,10 +1,16 @@
 import { Socket } from 'socket.io-client';
 
 const SOCKET_URL = 'http://localhost:3001';
+const SOCKET_MOCK = { emit: () => { }, on: () => { } };
 
 // eslint-disable-next-line import/no-mutable-exports
-export let ranaSocket: Socket;
+export let ranaSocket: Socket | typeof SOCKET_MOCK = SOCKET_MOCK;
 export const startListenRanaSocketIo = () => {
-  // @ts-ignore
-  ranaSocket = io(SOCKET_URL);
+  try {
+    // @ts-ignore
+    ranaSocket = io(SOCKET_URL);
+  } catch (e: unknown) {
+    if (typeof e === "string") alert(e);
+    else if (e instanceof Error) alert(e.message);
+  }
 };
