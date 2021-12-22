57afc9b8a9b8331c5b56eee9090915ec31e74b3c
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-22 15:02:49 +0300

    Update Empty component

--- packages/web/src/components/ui/Empty/index.tsx
+++ packages/web/src/components/ui/Empty/index.tsx
@@ -5,33 +5,53 @@ import cn from 'classnames';
 import styles from './Empty.module.css';
 
 type Props = {
+  error?: boolean;
   select?: boolean;
   size?: 's' | 'm' | 'l';
   text?: string;
   textOnly?: boolean;
 };
 
+// TODO: Refactory
 const Empty = ({
-  select = false, size = 'm', text = '', textOnly = false
+  error = false,
+  select = false,
+  size = 'm',
+  text = '',
+  textOnly = false,
 }: Props) => {
   // eslint-disable-next-line global-require
   const emptyIconSVG = require('./assets/undraw_empty_re_opql.svg').default;
   // eslint-disable-next-line global-require
   const selectIconSVG = require('./assets/undraw_selecting_re_5ff6.svg').default;
+  // eslint-disable-next-line global-require
+  const errorIconSvg = require('./assets/undraw_alert_re_j2op.svg').default;
 
   if (textOnly) {
     return (
       <div
         className={cn(styles.empty, styles[`size-${size}`], {
           [styles.textOnly]: textOnly,
-        })}
-      >
+        })}>
         {text && (
           <span className={cn(styles.title)}>
-            {text}
-            {' '}
-            {select ? '😏' : '😓'}
+            {text} {select ? '😏' : '😓'}
           </span>
+        )}
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className={cn(styles.empty, styles[`size-${size}`])}>
+        <div className={cn(styles.icon)}>
+          <ReactSVG className={cn({ [styles.withText]: text })} src={errorIconSvg} />
+        </div>
+        {text ? (
+          <span className={cn(styles.title)}>{text}</span>
+        ) : (
+          <span className={cn(styles.title)}>Oops, something went wrong</span>
         )}
       </div>
     );
@@ -43,13 +63,7 @@ const Empty = ({
         <div className={cn(styles.icon)}>
           <ReactSVG className={cn({ [styles.withText]: text })} src={selectIconSVG} />
         </div>
-        {text && (
-          <span className={cn(styles.title)}>
-            {text}
-            {' '}
-            😏
-          </span>
-        )}
+        {text && <span className={cn(styles.title)}>{text}</span>}
       </div>
     );
   }
@@ -59,13 +73,7 @@ const Empty = ({
       <div className={cn(styles.icon)}>
         <ReactSVG className={cn({ [styles.withText]: text })} src={emptyIconSVG} />
       </div>
-      {text && (
-        <span className={cn(styles.title)}>
-          {text}
-          {' '}
-          😓
-        </span>
-      )}
+      {text && <span className={cn(styles.title)}>{text}</span>}
     </div>
   );
 };
