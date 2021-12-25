1c75dbc69321fd67df519902fe82c3eb44a425b1
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-12-25 13:19:36 +0300

    Add Space component

---
+++ packages/web/src/components/ui/Space/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import cn from 'classnames';
+
+import styles from './Space.module.css';
+
+export enum SpaceSize {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
+
+export enum SpaceDirection {
+  Vertical = 'vertical',
+  Horizontal = 'horizontal',
+}
+
+type Props = {
+  children: React.ReactElement | React.ReactElement[];
+  size?: SpaceSize;
+  direction?: SpaceDirection;
+};
+
+const Space = ({ children, size, direction }: Props) => {
+  return (
+    <div
+      className={cn(
+        styles.space,
+        styles[`size-${size}`],
+        styles[`direction-${direction}`]
+      )}>
+      {children}
+    </div>
+  );
+};
+
+export default Space;
