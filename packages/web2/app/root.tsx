b0b1b7b186e349c87c913491c5022059571cfd46
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-01-07 16:41:28 +0300

    Add basic root file @rana-mc/web2

--- packages/web2/app/root.tsx
+++ packages/web2/app/root.tsx
@@ -1,32 +1,83 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+  useCatch,
+  useNavigate,
+} from 'remix';
+import type { MetaFunction } from 'remix';
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+// TODO: Replace by BackToHome component
+import { Button } from 'rsuite';
 
-export default function App() {
+import globalStyles from './styles/global.css';
+
+export const meta: MetaFunction = () => ({ title: 'RanaMC' });
+
+const App = () => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <Meta />
+      <Links />
+    </head>
+    <body>
+      <Outlet />
+      <ScrollRestoration />
+      <Scripts />
+      {process.env.NODE_ENV === 'development' && <LiveReload />}
+    </body>
+  </html>
+);
+
+export const CatchBoundary = () => {
+  const navigate = useNavigate();
+  const caught = useCatch();
+
+  const handleGoBack = () => {
+    navigate('../');
+  };
+
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Oops!</title>
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
+      <body className="not-found">
+        <h1>
+          {caught.status}
+          {' '}
+          {caught.statusText}
+        </h1>
+        <Button appearance="primary" onClick={handleGoBack}>
+          Go Home
+        </Button>
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
   );
-}
+};
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: globalStyles },
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+  {
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'use-credentials',
+  },
+  {
+    rel: 'stylesheet',
+    // eslint-disable-next-line max-len
+    href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap',
+  },
+];
+
+export default App;
