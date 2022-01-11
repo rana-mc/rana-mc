61b0c8ea81f6f62b6f7fcb412c5c7eba93a2f9e6
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-01-11 15:42:24 +0300

    Add fabricInstallers and fabricLoaders

--- packages/web2/app/components/ServerCoreBuilder/Fabric/index.tsx
+++ packages/web2/app/components/ServerCoreBuilder/Fabric/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useFetcher } from 'remix';
 import { RadioGroup, Stack, Radio } from 'rsuite';
 import { ValueType } from 'rsuite/esm/Radio';
 import SelectIcon from '~/components/SelectIcon';
@@ -7,48 +9,62 @@ type Props = {
   onBuild: (core: FabricCore) => void;
 };
 
-// TODO: Use loader and installer for build core
-const FabricCoreBuilder = ({ onBuild }: Props) => {
-  const cores: FabricCore[] = [
-    {
-      gameVersion: '123',
-      loader: {
-        separator: '',
-        build: 1,
-        maven: '',
-        version: '',
-        stable: true,
-      },
-      installer: {
-        url: '',
-        maven: '',
-        version: '',
-        stable: true,
-      },
-      serverInstallerUrl: '',
-    },
-  ];
+type FabricInstallersResponse = { data: FabricInstaller[]; success: boolean };
+type FabricLoadersResponse = { data: FabricLoader[]; success: boolean };
+
+const FabricCoreBuilder = ({ gameVersionId, onBuild }: Props) => {
+  const fabricInstallers = useFetcher<FabricInstallersResponse>();
+  const fabricLoaders = useFetcher<FabricLoadersResponse>();
+
+  useEffect(() => {
+    fabricInstallers.submit(null, {
+      action: 'test/api/fabricInstallers',
+      method: 'post',
+    });
+    fabricLoaders.submit(null, {
+      action: 'test/api/fabricLoaders',
+      method: 'post',
+    });
+  }, [gameVersionId]);
 
   const handleChange = (value: ValueType) => {
-    const core = cores.find((core) => core.serverInstallerUrl === value);
-    if (core) onBuild(core);
+    // const core = cores.find((core) => core.serverInstallerUrl === value);
+    // if (core) onBuild(core);
   };
 
   return (
-    <RadioGroup
-      className="coreVersionSelect"
-      inline
-      name="coreVersionSelect"
-      onChange={handleChange}>
-      {cores.map((core) => (
-        <Radio key={core.serverInstallerUrl} value={core.serverInstallerUrl}>
-          <Stack direction="row" spacing={8} alignItems="flex-start">
-            <SelectIcon name="minecraft" />
-            {core.installer.version} - {core.loader.version}
-          </Stack>
-        </Radio>
-      ))}
-    </RadioGroup>
+    <>
+      <RadioGroup
+        className="fabricInstallerSelect"
+        inline
+        name="fabricInstallerSelect"
+        onChange={handleChange}>
+        {fabricInstallers.data?.success &&
+          fabricInstallers.data.data.map((installer) => (
+            <Radio key={installer.version} value={installer.version}>
+              <Stack direction="row" spacing={8} alignItems="flex-start">
+                <SelectIcon name="minecraft" />
+                {installer.version}
+              </Stack>
+            </Radio>
+          ))}
+      </RadioGroup>
+      <RadioGroup
+        className="fabricLoaderSelect"
+        inline
+        name="fabricLoaderSelect"
+        onChange={handleChange}>
+        {fabricLoaders.data?.success &&
+          fabricLoaders.data.data.map((loader) => (
+            <Radio key={loader.version} value={loader.version}>
+              <Stack direction="row" spacing={8} alignItems="flex-start">
+                <SelectIcon name="minecraft" />
+                {loader.version}
+              </Stack>
+            </Radio>
+          ))}
+      </RadioGroup>
+    </>
   );
 };
 
---
+++ packages/web2/app/routes/test/api/fabricLoaders.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { ActionFunction, redirect } from "remix";
+
+export const loader = () => {
+  return redirect('/');
+};
+
+export const action: ActionFunction = async () => {
+  try {
+    const response = await axios.get('http://localhost:3001/api/fabric-loaders');
+    return { success: true, data: response.data }
+  } catch (err) {
+    return { success: false };
+  }
+};
