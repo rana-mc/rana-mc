faf5c6d397f0138588d72a9697812d5781ae7664
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-01-12 14:52:58 +0300

    Fix @rana-mc/cli

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,15 +1,27 @@
 import express from 'express';
 import path from 'path';
 import { startApiServer } from '@rana-mc/api';
+import { createRequestHandler } from '@remix-run/express';
+
+// @ts-ignore
+import build from '@rana-mc/web/build';
 
 const WEB_PORT = 3000;
 
 const startWebServer = async () => {
   const app = express();
-  const webPath = path.resolve('.', 'node_modules/@rana-mc/web/public/build');
 
-  console.log(`RanaMC web path: ${webPath}`);
-  app.use(express.static(webPath));
+  console.log(`RanaMC web path: ${build}`);
+  app.all(
+    "*",
+    createRequestHandler({
+      build,
+      getLoadContext(req, res) {
+        // FYI: Maybe later add context of host machine
+        return {};
+      }
+    })
+  );
 
   app.listen(WEB_PORT, () => {
     console.log(`Working on ${WEB_PORT} port...`);
