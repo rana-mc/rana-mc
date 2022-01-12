5c5a57db757aad23153bd2b69d67c1a4c12e6844
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-01-12 13:52:18 +0300

    Correct use Socket.client.io

--- packages/web2/app/vendors/ranaSocketIo.ts
+++ packages/web2/app/vendors/ranaSocketIo.ts
@@ -1,21 +1,7 @@
-import { Socket } from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 const SOCKET_URL = 'http://localhost:3001';
-const SOCKET_MOCK = { emit: () => { }, on: () => { } };
-
-// eslint-disable-next-line import/no-mutable-exports
-export let ranaSocket: Socket | typeof SOCKET_MOCK = SOCKET_MOCK;
-export const startListenRanaSocketIo = () => {
-  try {
-    // @ts-ignore
-    ranaSocket = io(SOCKET_URL);
-  } catch (e: unknown) {
-    // eslint-disable-next-line no-alert
-    if (typeof e === 'string') alert(e);
-    // eslint-disable-next-line no-alert
-    else if (e instanceof Error) alert(e.message);
-  }
-};
+export const ranaSocket = io(SOCKET_URL);
 
 // TODO: Use from @rana-mc/types
 export enum ServerActions {
